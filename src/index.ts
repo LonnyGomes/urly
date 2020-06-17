4a76290af911eec3bff65c09496058d7377bd2ff
Lonny Gomes <code@lonnygomes.com>
2020-06-17 14:39:48 -0400

    feat: incorporate base koa server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-console.log('hello world');
-
 import * as dotenv from 'dotenv';
+import { Context, Next } from 'koa';
 
 dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const Koa = require('koa');
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx: Context, next: Next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx: Context, next: Next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async (ctx: Context) => {
+    ctx.body = 'Hello World';
+});
+
+console.log(`Server started on on port ${PORT}`);
+
+app.listen(PORT);
