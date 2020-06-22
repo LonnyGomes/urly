a67a908c7e2fdc41e0f412623b2e6003cfc03f9b
Lonny Gomes <code@lonnygomes.com>
2020-06-21 23:25:24 -0400

    chore: add starter unit tests for randNum and genHash functions
    refs #4

--- test/shortener.test.ts
+++ test/shortener.test.ts
@@ -6,6 +6,41 @@ describe('Shortener', () => {
         shortener = new Shortener();
     });
 
+    describe('genHash', () => {
+        const hashChars = Shortener.ACCEPTABLE_CHARS;
+
+        it('should generate hash of specified length', () => {
+            let hashLen = 4;
+            let results = shortener.genHash(hashLen, hashChars);
+            expect(results.length).toEqual(hashLen);
+
+            hashLen = 10;
+            results = shortener.genHash(hashLen, hashChars);
+            expect(results.length).toEqual(hashLen);
+        });
+    });
+
+    describe('randNum', () => {
+        it('should return random number between min and max values', () => {
+            // NOTE: since the result is number we want to test multiple times
+            // across a small sample set
+            const testCount = 30;
+
+            const minVal = 5;
+            const maxVal = 10;
+            let results: number;
+
+            // cycle through this function several times to
+            // reduce the entropy
+            for (let idx = 0; idx < testCount; idx += 1) {
+                results = shortener.randNum(minVal, maxVal);
+
+                expect(results).toBeGreaterThanOrEqual(minVal);
+                expect(results).toBeLessThanOrEqual(maxVal);
+            }
+        });
+    });
+
     describe('shorten', () => {
         it('should throw an error when an invalid URL is supplied', () => {
             const expectedErrorMsg = 'Invalid URL supplied';
