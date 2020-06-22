8aefe155d574aae459221b9e0b1af03718e1125b
Lonny Gomes <code@lonnygomes.com>
2020-06-21 23:05:02 -0400

    chore: incorporate @panoz7's code into Shortener class
    refs #4

--- src/utils/shortener.ts
+++ src/utils/shortener.ts
@@ -1,4 +1,26 @@
 export class Shortener {
+    private ACCEPTABLE_CHARS = 'abcdefghjkmnpqrstuvwxyz23456789';
+    private HASH_LEN = 7;
+
+    private genHash(length: number, acceptableCharacters: string) {
+        // Build an array of random characters
+        const randomChars = [];
+        for (let i = 0; i < length; i++) {
+            randomChars.push(
+                acceptableCharacters[
+                    this.randNum(0, acceptableCharacters.length - 1)
+                ]
+            );
+        }
+        // Join the characters together and return them
+        return randomChars.join('');
+    }
+
+    private randNum(min: number, max: number) {
+        const dif = max - min;
+        return Math.round(Math.random() * dif) + min;
+    }
+
     public shorten(url: string): string {
         if (!url) {
             throw new Error('URL not supplied');
