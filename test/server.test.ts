38c946cd5355857ae9e0b5244690147ff71af058
Lonny Gomes <code@lonnygomes.com>
2020-06-30 15:30:03 -0400

    wip: update api unit test to use test db
    refs #24

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,10 +1,28 @@
 import request from 'supertest';
-import { app } from '../src/server';
+import { initServer } from '../src/server';
+import { UrlyDatabaseConnection } from '../src/db/db-connection';
+import * as path from 'path';
+import * as fs from 'fs-extra';
+
+const FIXTURE_PATH = path.resolve(__dirname, 'fixtures');
+const DB_PATH = path.resolve(FIXTURE_PATH, 'db', 'urly.db');
+const TMP_PATH = path.resolve(FIXTURE_PATH, 'tmp');
+const TMP_DB_PATH = path.resolve(TMP_PATH, 'urly.db');
 
 describe('koa server', () => {
     let server: any;
+
     beforeEach(() => {
+        fs.ensureDirSync(TMP_PATH);
+        fs.copyFileSync(DB_PATH, TMP_DB_PATH);
+
+        const db = new UrlyDatabaseConnection(TMP_DB_PATH);
+        const app = initServer(db);
         server = app.callback();
+    });
+
+    afterEach(() => {
+        fs.removeSync(TMP_PATH);
     });
 
     describe('app', () => {
@@ -24,6 +42,24 @@ describe('koa server', () => {
             expect(response.header.location).toMatch(/404.html$/);
             expect(response.type).toEqual('text/html');
             expect(response.text).toMatch(/^Redirecting to/);
+        });
+    });
+
+    describe('/api', () => {
+        describe('GET /url:fullUrl', () => {
+            it('should return a URL when a valid hash is supplied', async () => {
+                const expectedHashKey = 'abcd123';
+                const expectedResponse = {
+                    status: true,
+                    fullUrl: 'https://www.google.com',
+                };
+                const response: any = await request(server).get(
+                    `/api/url/${expectedHashKey}`
+                );
+
+                expect(response.status).toEqual(200);
+                expect(response.body).toEqual(expectedResponse);
+            });
         });
     });
 });
